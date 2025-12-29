b1550494ceb35fbcfa5aba36e43127dc614ecdd3
José Luis Espinoza <joseluisessa@gmail.com>
2025-12-28 20:18:55 -0600

    Añadido del modelo vista controlador de la tabla de Tipos

--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 
-import persons from '../routes/persons.js';
+import persons from '../routes/persons.js'; // personas
+import types from '../routes/types.js'; // tipos
+
+//movimientos
 
 export default function (app){
   app.use(express.json());  
     
   app.use('/api/persons', persons);
+  app.use('/api/types', types);
 }
